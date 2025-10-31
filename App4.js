e7a3532ad1b1bf8cc63d943e69c11c9e277e7d36
vigilanteX <dixitaman8@gmail.com>
2025-10-31 19:33:53 +0530

    root.render(): ovverides the content of div where id='root'

---
+++ App4.js
@@ -0,0 +1,4 @@
+let root=ReactDOM.createRoot(document.getElementById("root"));
+let element=React.createElement('h1',{},'Namaste React using ReactDOM v18');
+//THIS WILL OVERIDE THE CONTENT OF THE DIV WITH ID ROOT
+root.render(element);
