e1b2c306d76c33edd40812016cdd1ae19150e13b
vigilanteX <dixitaman8@gmail.com>
2025-11-04 19:21:38 +0530

    Parcel doc-https://parceljs.org/

--- Episode2/App.js
+++ Episode2/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-const element=React.createElement('h1',{},'HELLO WORLD')
+const element=React.createElement('h1',{key:1},'HELLO WORLD')
+const element2=React.createElement('h1',{key:2},'HELLO WORLD')
+const divElement=React.createElement('div',{},[element,element2])
 const root=ReactDOM.createRoot(document.getElementById('root'))
-root.render(element)
+root.render(divElement)
