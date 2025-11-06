b6c3ea64a3b4871d6532a8de389980ad5087ada7
vigilanteX <dixitaman8@gmail.com>
2025-11-06 19:52:44 +0530

    adding prod build command in script. And comparing JSX and React.createElement()

---
+++ Episode2/App2.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//React.createElement creates an object which is called a React Element not a HTML element
+//React.createElement=>object=>HTML element (rendered using ReactDOM);
+const heading=React.createElement("h1",{},"Namaste React 🚀");
+const heading2=<h1>Hello jsx react</h1>
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading2);
