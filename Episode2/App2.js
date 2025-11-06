7fe0dd9a8b024723eea36036d28c948e2d89846d
vigilanteX <dixitaman8@gmail.com>
2025-11-06 20:16:42 +0530

    babel code comment-https://babeljs.io/

--- Episode2/App2.js
+++ Episode2/App2.js
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 //React.createElement creates an object which is called a React Element not a HTML element
 //React.createElement=>object=>HTML element (rendered using ReactDOM);
 const heading=React.createElement("h1",{},"Namaste React 🚀");
+//JSX(transpiled before it reaches the js)->parcel->babel->converts to code that js engine understand
+//JSX->calls React.createElement()->creates an object which is called a React Element not a HTML element
 const heading2=<h1>Hello jsx react</h1>
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading2);
